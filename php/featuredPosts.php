b4f2257c2dbdb1ec25248b635da76c7aeaef44a8
Sandra Kupfer <kupfer.sandra.m@gmail.com>
2021-05-14 14:27:20 -0400

    Featured posts section.

---
+++ php/featuredPosts.php
@@ -0,0 +1,87 @@
+<?php
+
+use php\utl\{DB};
+require_once "utl/Database.php";
+
+
+$featuredPosts = DB::getDbResult( "SELECT * FROM featuredPosts where postId=1" );
+var_dump( $featuredPosts );
+
+
+
+$title = "";
+$description = "";
+$tags = "";
+$link = "";
+$image = "";
+
+
+
+?>
+
+
+<!-- Recent Posts Start -->
+    <section class="recent-posts">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="single-rpost d-sm-flex align-items-center" data-aos="fade-right" data-aos-duration="800">
+                        <div class="post-content text-sm-right">
+                            <time datetime="2019-04-06T13:53">15 Oct, 2019</time>
+                            <h3><a href="#">Proudly for us to build stylish</a></h3>
+                            <p><a href="#">Seanding</a>, <a href="#">Website</a>, <a href="#">E-commerce</a></p>
+                            <a class="post-btn" href="#"><i class="fa fa-arrow-right"></i></a>
+                        </div>
+                        <div class="post-thumb">
+                            <img class="img-fluid" src="assets/images/post1.jpg" alt="Post 1">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="single-rpost d-sm-flex align-items-center" data-aos="fade-left" data-aos-duration="800">
+                        <div class="post-thumb">
+                            <img class="img-fluid" src="assets/images/post2.jpg" alt="Post 1">
+                        </div>
+                        <div class="post-content">
+                            <time datetime="2019-04-06T13:53">15 Oct, 2019</time>
+                            <h3><a href="#">Remind me to water the plants</a></h3>
+                            <p><a href="#">Seanding</a>, <a href="#">Website</a>, <a href="#">E-commerce</a></p>
+                            <a class="post-btn" href="#"><i class="fa fa-arrow-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="single-rpost d-sm-flex align-items-center" data-aos="fade-right" data-aos-delay="200"
+                        data-aos-duration="800">
+                        <div class="post-content text-sm-right">
+                            <time datetime="2019-04-06T13:53">15 Oct, 2019</time>
+                            <h3><a href="#">Add apples to the grocery list</a></h3>
+                            <p><a href="#">Seanding</a>, <a href="#">Website</a>, <a href="#">E-commerce</a></p>
+                            <a class="post-btn" href="#"><i class="fa fa-arrow-right"></i></a>
+                        </div>
+                        <div class="post-thumb">
+                            <img class="img-fluid" src="assets/images/post3.jpg" alt="Post 1">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="single-rpost d-sm-flex align-items-center" data-aos="fade-left" data-aos-delay="200"
+                        data-aos-duration="800">
+                        <div class="post-thumb">
+                            <img class="img-fluid" src="assets/images/post4.jpg" alt="Post 1">
+                        </div>
+                        <div class="post-content">
+                            <time datetime="2019-04-06T13:53">15 Oct, 2019</time>
+                            <h3><a href="#">Make it warmer downstairs</a></h3>
+                            <p><a href="#">Seanding</a>, <a href="#">Website</a>, <a href="#">E-commerce</a></p>
+                            <a class="post-btn" href="#"><i class="fa fa-arrow-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="text-center">
+                <a href="#" class="btn btn-primary">See More</a>
+            </div>
+        </div>
+    </section>
+    <!-- Recent Posts End -->
