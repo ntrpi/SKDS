2b92bf78e6e10c03e5b0b82272fa5bf36fb9e379
Sandra Kupfer <kupfer.sandra.m@gmail.com>
2021-05-26 15:14:03 -0400

    Darn it.

--- php/utl/Database.php
+++ php/utl/Database.php
@@ -9,7 +9,7 @@ namespace skds\php\utl;
         private static $host = "158.69.17.240:3306";
         private static $database = "skdssite_site";
         private static $userName = "skdssite_me";
-        private static $password = "Ent1!ropy";
+        private static $password = "";
 
         private static $dbPdo;
     
