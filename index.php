44d05caba75580630c2458e97edf3cfee1213d70
Sandra Kupfer <kupfer.sandra.m@gmail.com>
2021-06-13 12:55:52 -0400

    Accessibility.

--- index.php
+++ index.php
@@ -25,105 +25,110 @@ TODO: Restaurant
 
 <!doctype html>
 <html lang="en">
+
 <head>
-    <?php include_once "./php/head.php"; ?>
-    <!-- Document title -->
-    <title>SKDS Home</title>
+  <?php include_once "./php/head.php"; ?>
+  <!-- Document title -->
+  <title>SKDS Home</title>
 </head>
+
 <body style="overflow-x: hidden;">
 
-    <?php include_once "./php/header.php"; ?>
+  <?php include_once "./php/header.php"; ?>
 
+  <main>
     <!-- Call To Action Start -->
     <section class="cta" data-aos="fade-up" data-aos-delay="0">
-        <div class="container">
-            <div class="cta-content d-xl-flex align-items-center justify-content-around text-center text-xl-left">
-                <div class="content text-center" data-aos="fade-right" data-aos-delay="200">
-                    <h2>ACCOMPLISHED SOFTWARE DEVELOPER</h2>
-                    <p>Over fifteen years of software development experience.</p>
-                </div>
-                <div class="subscribe-btn" data-aos="fade-left" data-aos-delay="400" data-aos-offset="0">
-                    <a href="docs/SANDRA KÜPFER.pdf" download="SANDRA KÜPFER.pdf" class="btn btn-primary">Download Resume</a>
-                </div>
-            </div>
+      <div class="container">
+        <div class="cta-content d-xl-flex align-items-center justify-content-around text-center text-xl-left">
+          <div class="content text-center" data-aos="fade-right" data-aos-delay="200">
+            <h2>ACCOMPLISHED SOFTWARE DEVELOPER</h2>
+            <p>Over fifteen years of software development experience.</p>
+          </div>
+          <div class="subscribe-btn" data-aos="fade-left" data-aos-delay="400" data-aos-offset="0">
+            <a href="docs/SANDRA KÜPFER.pdf" download="SANDRA KÜPFER.pdf" class="btn btn-primary">Download Resume</a>
+          </div>
         </div>
+      </div>
     </section>
     <!-- Call To Action End -->
 
-    <div class="title text-center mt-5 mb-4"  data-aos="fade-up" data-aos-delay="100" id="projects">
+    <!-- Projects Start -->
+    <section>
+      <div class="title text-center mt-5 mb-4" data-aos="fade-up" data-aos-delay="100" id="projects">
         <h2 class="title-blue ">Projects</h2>
-    </div>
+      </div>
 
-    <!-- Featured Start -->
-    <section class="featured">
+      <!-- Manitouadge Start -->
+      <div class="featured">
         <div class="container">
-            <div class="title">
-                <h6 class="title-primary">Website Redesign Group Project</h6>
-                <h2 class="title-blue">Manitouadge General Hospital</h2>
+          <div class="title">
+            <h3 class="title-primary h6">Website Redesign Group Project</h3>
+            <h4 class="title-blue h2">Manitouadge General Hospital</h4>
+          </div>
+          <div class="row">
+            <div class="col-md-6" data-aos="fade-right" data-aos-delay="400" data-aos-duration="800">
+              <p>This project is the result of the work from several second semester classes of the Humber Web Development program. The goal is to showcase our ability to envision, plan, and implement a large-scale project with real-world application.</p>
+              <a href="http://skds.us-east-2.elasticbeanstalk.com/" class="btn btn-primary">Website</a>
+              <a href="https://github.com/ntrpi/ManitouadgeHospitalProject" class="btn btn-primary">Github</a>
+              <a href="https://drive.google.com/drive/folders/1Q3Dks1G-jq4-UOpELw_8PRKidU6pBUc3?usp=sharing" class="btn btn-primary">Docs</a>
             </div>
-            <div class="row">
-                <div class="col-md-6" data-aos="fade-right" data-aos-delay="400" data-aos-duration="800">
-                    <p>This project is the result of the work from several second semester classes of the Humber Web Development program. The goal is to showcase our ability to envision, plan, and implement a large-scale project with real-world application.</p>
-                    <a href="http://skds.us-east-2.elasticbeanstalk.com/" class="btn btn-primary">Website</a>
-                    <a href="https://github.com/ntrpi/ManitouadgeHospitalProject" class="btn btn-primary">Github</a>
-                    <a href="https://drive.google.com/drive/folders/1Q3Dks1G-jq4-UOpELw_8PRKidU6pBUc3?usp=sharing" class="btn btn-primary">Docs</a>
-                </div>
-                <div class="col-md-6" data-aos="fade-left" data-aos-delay="400" data-aos-duration="800">
-                    <div class="featured-img">
-                        <img class="featured-big" src="img/manitouadge/home3.png" alt="Featured 1">
-                        <img class="featured-small" src="img/manitouadge/home1.png" alt="Featured 2">
-                    </div>
-                </div>
+            <div class="col-md-6" data-aos="fade-left" data-aos-delay="400" data-aos-duration="800">
+              <div class="featured-img">
+                <img class="featured-big" src="img/manitouadge/home3.png" alt="Featured 1">
+                <img class="featured-small" src="img/manitouadge/home1.png" alt="Featured 2">
+              </div>
             </div>
+          </div>
         </div>
-    </section>
-    <!-- Featured End -->
+      </div>
+      <!-- Manitouadge End -->
 
-    <!-- Trust Start -->
-    <section class="trust">
+      <!-- Art Gallery Start -->
+      <div class="trust">
         <div class="container">
-            <div class="row">
-                <div class="col-md-5" data-aos="fade-right" data-aos-delay="200" data-aos-duration="800">
-                    <div class="title">
-                        <h6 class="title-primary">ASP.NET Passion Project</h6>
-                        <h2>Art Gallery</h2>
-                    </div>
-                    <p>For this project we were encouraged to create an app about something we have a personal interest in. I started painting in spring of 2020 and I have found that I love it. I created this app as a way to showcase my work, and eventually sell it. Both the app and painting are a work in progress.</p>
-                    <h5>Design & Development</h5>
-                    <ul class="list-unstyled">
-                        <li><a href="http://art-gallery-env.eba-pfm5mwmt.us-east-2.elasticbeanstalk.com/">Website</a></li>
-                        <li><a href="https://docs.google.com/document/d/1VeD4BA_O6KAwlSA7q8AKKi96fWe1Rgdt_OUIhPCXpaM/edit?usp=sharing">Docs</a></li>
-                        <li><a href="https://github.com/ntrpi/ArtGallery">Github</a></li>
-                    </ul>
-                </div>
-                <div class="col-md-5 gallery">
-                    <div class="row no-gutters">
-                        <div class="w-50 h-100 gal-img-x" data-aos="fade-up"
-                            data-aos-delay="200" data-aos-duration="400">
-                            <img class="img-fluid" src="img/artGallery/canvas1_2.jpg" alt="Website screenshot.">
-                        </div>
-                        <div class="w-50 h-50 gal-img-x" data-aos="fade-up"
-                            data-aos-delay="400" data-aos-duration="600">
-                            <!-- <img class="img-fluid" src="img/artGallery/home2.png" alt="Website screenshot.">
-                            <div data-aos="fade-up" data-aos-delay="0" data-aos-duration="600"> -->
-                                <img class="img-fluid" src="img/artGallery/canvas3.png" alt="Website screenshot.">
-                            <!-- </div> -->
-                        </div>
-                    </div>
-                </div>
+          <div class="row">
+            <div class="col-md-5" data-aos="fade-right" data-aos-delay="200" data-aos-duration="800">
+              <div class="title">
+                <h3 class="title-primary h6">ASP.NET Passion Project</h3>
+                <h4 class="h3">Art Gallery</h4>
+              </div>
+              <p>For this project we were encouraged to create an app about something we have a personal interest in. I started painting in spring of 2020 and I have found that I love it. I created this app as a way to showcase my work, and eventually sell it. Both the app and painting are a work in progress.</p>
+              <h5>Design & Development</h5>
+              <ul class="list-unstyled">
+                <li><a href="http://art-gallery-env.eba-pfm5mwmt.us-east-2.elasticbeanstalk.com/">Website</a></li>
+                <li><a href="https://docs.google.com/document/d/1VeD4BA_O6KAwlSA7q8AKKi96fWe1Rgdt_OUIhPCXpaM/edit?usp=sharing">Docs</a></li>
+                <li><a href="https://github.com/ntrpi/ArtGallery">Github</a></li>
+              </ul>
             </div>
+            <div class="col-md-5 gallery">
+              <div class="row no-gutters">
+                <div class="w-50 h-100 gal-img-x" data-aos="fade-up" data-aos-delay="200" data-aos-duration="400">
+                  <img class="img-fluid" src="img/artGallery/canvas1_2.jpg" alt="Website screenshot.">
+                </div>
+                <div class="w-50 h-50 gal-img-x" data-aos="fade-up" data-aos-delay="400" data-aos-duration="600">
+                  <!-- <img class="img-fluid" src="img/artGallery/home2.png" alt="Website screenshot.">
+                                <div data-aos="fade-up" data-aos-delay="0" data-aos-duration="600"> -->
+                  <img class="img-fluid" src="img/artGallery/canvas3.png" alt="Website screenshot.">
+                  <!-- </div> -->
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
+      <!-- Art Gallery End -->
+
     </section>
-    <!-- Trust End -->
-
-
+    <!-- Projects End -->
 
     <?php include_once "./php/featuredPosts.php"; ?>
 
-    <?php include_once "./php/footer.php"; ?>
+  </main>
 
-    <?php include_once "./php/bodyScripts.php"; ?>
+  <?php include_once "./php/footer.php"; ?>
+
+  <?php include_once "./php/bodyScripts.php"; ?>
 </body>
+
 </html>
-
-
--- php/footer.php
+++ php/footer.php
@@ -1,165 +1,172 @@
 <?php
 
-use skds\php\Models\{Project, Image, ProjectImage};
+
+use skds\php\Models\Image;
+use skds\php\Models\Project;
+use skds\php\Models\ProjectImage;
+
 require_once "Models/Project.php";
 require_once "Models/Image.php";
 require_once "Models/ProjectImage.php";
 
 $projectHelper = new Project();
-$projects = $projectHelper->getProjects();
+$projects      = $projectHelper->getProjects();
 
-$projectDivs = array();
-$imageHelper = new Image();
+$projectDivs        = array();
+$imageHelper        = new Image();
 $projectImageHelper = new ProjectImage();
-$counter = 0;
-foreach( $projects as $proj ) {
+$counter            = 0;
+foreach( $projects as $proj ) {if ( $counter == 4 ) {break;
+	}
+	$counter++;
 
-    if( $counter == 4 ) {
-        break;
-    }
-    $counter++;
+	$projectId   = $proj->projectId;
+	$projectName = $proj->name;
+	$projectLink = $proj->link;
 
-    $projectId = $proj->projectId;
-    $projectName = $proj->name;
-    $projectLink = $proj->link;
+	$projectImages = $projectImageHelper->getProjectImagesWhere( "projectId", $projectId );
+	$imageId       = 0;
+	$orderNumber   = 2;
+	foreach( $projectImages as $projImg ) {if ( $projImg->orderNumber < $orderNumber ) {$imageId     = $projImg->imageId;
+			$orderNumber = $projImg->orderNumber;
+		}
+	}
 
-    $projectImages = $projectImageHelper->getProjectImagesWhere( "projectId", $projectId );
-    $imageId = 0;
-    $orderNumber = 2;
-    foreach( $projectImages as $projImg ) {
-        if( $projImg->orderNumber < $orderNumber ) {
-            $imageId = $projImg->imageId;
-            $orderNumber = $projImg->orderNumber;
-        }
-    }
+	$image    = $imageHelper->getImage( $imageId );
+	$imageSrc = $image->path;
+	$altText  = $image->altText;
 
-    $image = $imageHelper->getImage( $imageId );
-    $imageSrc = $image->path;
-    $altText = $image->altText;
-    
-    $div = "
+	$div = "
     <div class=\"media\">
         <a class=\"rcnt-img\" href=\"{$projectLink}\"><img src=\"{$imageSrc}\" alt=\"{$altText}\"></a>
         <div class=\"media-body ml-3\">
-            <h6><a href=\"{$projectLink}\">{$projectName}</a></h6>
+            <h4 class=\"h6\"><a href=\"{$projectLink}\">{$projectName}</a></h4>
         </div>
     </div>";
 
-    array_push( $projectDivs, $div );
+	array_push( $projectDivs, $div );
 }
 ?>
 
 <!-- Footer Start -->
-    <footer>
-        <!-- Widgets Start -->
-        <div class="footer-widgets">
-            <div class="container">
+<footer>
+  <!-- Widgets Start -->
+  <div class="footer-widgets">
+    <div class="container">
+      <div class="row">
 
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="single-widget contact-widget" data-aos="fade-up" data-aos-delay="0">
-                            <h6 class="widget-title text-uppercase">Sandra K&uuml;pfer</h6>
-                            <div class="testimonials p-3 pt-5">
-                                <div class="test-img" data-aos="fade-up" data-aos-delay="0" data-aos-offset="0">
-                                    <img src="img/headshots/HeadShot1.png" alt="Headshot of Sandra Kupfer.">
-                                </div>
-                            </div>
-                            <h6 class="widget-title">&nbsp;</h6>
-                            <div class="media">
-                                <i class="fa fa-map-marker"></i>
-                                <div class="media-body ml-3">
-                                    <h6>Address</h6>
-                                    9-423 Westwood Dr.<br>
-                                    Kitchener, ON
-                                </div>
-                            </div>
-                            <div class="media">
-                                <i class="fa fa-envelope-o"></i>
-                                <div class="media-body ml-3">
-                                    <h6>Email</h6>
-                                    <a href="mailto:kupfer.sandra.m@gmail.com" style="text-decoration: none;"><div class="d-inline-block">kupfer.sandra.m</div><div class="d-inline">@gmail.com</div></a>
-                                </div>
-                            </div>
-                            <div class="media">
-                                <i class="fa fa-phone"></i>
-                                <div class="media-body ml-3">
-                                    <h6>Phone</h6>
-                                    <a href="tel:+610791803458"> +1 (519) 573-8255</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="subscribe-widget">
-                            <ul class="nav social-nav ml-5">
-                                <li><a href="https://www.linkedin.com/in/sandra-kupfer/"><i class="fa fa-linkedin mt-2"></i></a></li>
-                                <li><a href="https://github.com/ntrpi"><i class="fa fa-github mt-2"></i></a></li>
-                            </ul>
-                        </div>
-                </div>
-                    <div class="col-md-4">
-                        <div class="single-widget recent-post-widget" data-aos="fade-up" data-aos-delay="400">
-                            <h6 class="widget-tiltle">Projects</h6>
-                            <?php
-                                foreach( $projectDivs as $div ) {
-                                    echo $div;
-                                }
-                            ?>
-                        </div>
-                    </div>
-                    <div class="col-md-4" id="contact">
-                        <div class="single-widget" data-aos="fade-up" data-aos-delay="800">
-                            <h6 class="widget-tiltle">Contact Me</h6>
-                            <form class="contact-form" method="get">
-                                <div class="subscribe-widget">
-                                    <div class="input-group my-3">
-                                        <label for="contactName" class="sr-only">Your name</label>
-                                        <input class="form-control" name="contactName" id="contactName" type="text" placeholder="Your Name">
-                                        <span class="accent"></span>
-                                    </div>
-                                    <div class="input-group my-3">
-                                        <label for="contactEmail" class="sr-only">Your email</label>
-                                        <input class="form-control" name="contactEmail" id="contactEmail" type="email" placeholder="Your Email">
-                                        <span class="accent"></span>
-                                    </div>
-                                    <div class="input-group my-3">
-                                        <label for="contactMessage" class="sr-only">Your message</label>
-                                        <textarea class="form-control" name="contactMessage" id="contactMessage" cols="30" rows="10" placeholder="Your Message"></textarea></textarea>
-                                        <span class="accent"></span>
-                                    </div>
-                                </div>
-                                <div class="input-group my-3">
-                                    <input class="btn btn-primary" type="submit" value="Send">
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+        <section class="col-md-4">
+          <div class="single-widget contact-widget" data-aos="fade-up" data-aos-delay="0">
+            <h3 class="widget-title text-uppercase h6">Sandra K&uuml;pfer</h3>
+            <div class="testimonials p-3 pt-5">
+              <div class="test-img" data-aos="fade-up" data-aos-delay="0" data-aos-offset="0">
+                <img src="img/headshots/HeadShot1.png" alt="Headshot of Sandra Kupfer.">
+              </div>
             </div>
-        </div>
-        <!-- Widgets End -->
-        <!-- Foot Note Start -->
-        <div class="foot-note">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="footer-content text-center text-lg-left d-lg-flex justify-content-between align-items-center">
-                            <p class="mb-0" data-aos="fade-right" data-aos-offset="0">&copy; 2021 All Rights Reserved.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="footer-content text-center text-lg-left d-lg-flex justify-content-between align-items-center">
-                            <p class="mb-0"><a href="./privacy.php">Privacy Policy</a></p>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="footer-content text-center text-lg-left d-lg-flex justify-content-between align-items-center">
-                            <p class="mb-0" data-aos="fade-left" data-aos-offset="0"><a href="sitemap.html">Sitemap</a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
+            <h6 class="widget-title">&nbsp;</h6>
+            <div class="media">
+              <i class="fa fa-map-marker mr-2" aria-hidden="true"></i>
+              <div class="media-body ml-3">
+                <h4 class="h6">Address</h4>
+                <span>
+                9-423 Westwood Dr.<br>
+                Kitchener, ON
+                </span>
+              </div>
             </div>
+            <div class="media">
+              <i class="fa fa-envelope-o" aria-hidden="true"></i>
+              <div class="media-body ml-3">
+                <h4 class="h6">Email</h4>
+                <a href="mailto:kupfer.sandra.m@gmail.com" style="text-decoration: none;">
+                  <div class="d-inline-block">kupfer.sandra.m</div>
+                  <div class="d-inline">@gmail.com</div>
+                </a>
+              </div>
+            </div>
+            <div class="media">
+              <i class="fa fa-phone mr-1" aria-hidden="true"></i>
+              <div class="media-body ml-3">
+                <h4 class="h6">Phone</h4>
+                <a href="tel:+610791803458"> +1 (519) 573-8255</a>
+              </div>
+            </div>
+          </div>
+          <div class="subscribe-widget">
+            <ul class="nav social-nav ml-5">
+              <li><a href="https://www.linkedin.com/in/sandra-kupfer/" aria-label="Github"><i class="fa fa-linkedin mt-2" aria-hidden="true"></i></a></li>
+              <li><a href="https://github.com/ntrpi" aria-label="Github"><i class="fa fa-github mt-2" aria-hidden="true"></i></a></li>
+            </ul>
+          </div>
+        </section>
+
+        <section class="col-md-4">
+          <div class="single-widget recent-post-widget" data-aos="fade-up" data-aos-delay="400">
+            <h3 class="widget-title h6">Projects</h3>
+            <?php
+            foreach($projectDivs as $div) {
+              echo $div;
+            }
+            ?>
+          </div>
+        </section>
+
+        <section class="col-md-4" id="contact">
+          <div class="single-widget" data-aos="fade-up" data-aos-delay="800">
+            <h4 class="widget-title h6">Contact Me</h4>
+            <form class="contact-form" method="get">
+              <div class="subscribe-widget">
+                <div class="input-group my-3">
+                  <label for="contactName" class="sr-only">Your name</label>
+                  <input class="form-control" name="contactName" id="contactName" type="text" placeholder="Your Name">
+                  <span class="accent"></span>
+                </div>
+                <div class="input-group my-3">
+                  <label for="contactEmail" class="sr-only">Your email</label>
+                  <input class="form-control" name="contactEmail" id="contactEmail" type="email" placeholder="Your Email">
+                  <span class="accent"></span>
+                </div>
+                <div class="input-group my-3">
+                  <label for="contactMessage" class="sr-only">Your message</label>
+                  <textarea class="form-control" name="contactMessage" id="contactMessage" cols="30" rows="10" placeholder="Your Message"></textarea></textarea>
+                  <span class="accent"></span>
+                </div>
+              </div>
+              <div class="input-group my-3">
+                <input class="btn btn-primary" type="submit" value="Send">
+              </div>
+            </form>
+          </div>
+        </section>
+
+      </div>
+    </div>
+  </div>
+  <!-- Widgets End -->
+  <!-- Foot Note Start -->
+  <div class="foot-note">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="footer-content text-center text-lg-left d-lg-flex justify-content-between align-items-center">
+            <p class="mb-0" data-aos="fade-right" data-aos-offset="0">&copy; 2021 All Rights Reserved.
+            </p>
+          </div>
         </div>
-        <!-- Foot Note End -->
-    </footer>
-    <!-- Footer Endt -->
+        <div class="col-md-4">
+          <div class="footer-content text-center text-lg-left d-lg-flex justify-content-between align-items-center">
+            <p class="mb-0"><a href="./privacy.php">Privacy Policy</a></p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="footer-content text-center text-lg-left d-lg-flex justify-content-between align-items-center">
+            <p class="mb-0" data-aos="fade-left" data-aos-offset="0"><a href="sitemap.html">Sitemap</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Foot Note End -->
+</footer>
+<!-- Footer Endt -->
